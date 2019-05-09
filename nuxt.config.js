2d94fedf395dc915487410780df618c6ce31b072
BenStirrup <benjamin.stirrup@gmail.com>
2019-05-10 00:11:38 +0200

    Add manifest for PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,15 +3,47 @@ import pkg from './package'
 export default {
   mode: 'universal',
 
+  /* PWA manifest */
+  manifest: {
+    name: 'B.S. Web Development',
+    short_name: 'B.S. Web',
+    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    icons: [
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png'
+      },
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  },
+
   /* Headers of the page */
   head: {
-    title: pkg.name,
+    title: 'Web development by Benjamin Stirrup',
     meta: [
+      { author: 'Benjamin Stirrup ' },
       { charset: 'utf-8' },
+      { description: 'Professional web development by Benjamin Stirrup' },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { lang: 'en' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { theme_color: '#ffffff' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   /* Customize the progress-bar color */
