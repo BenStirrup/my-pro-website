59a3868e2bf1e458bf9f600550bfb1d503e9af10
BenStirrup <benjamin.stirrup@gmail.com>
2019-05-10 20:46:24 +0200

    Refacto top navbar to be fixed and have onl a white version

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,40 +1,31 @@
 <template>
   <div class="navbar-container" :class="{ responsive: showResponsiveMenu }">
-    <template v-if="showResponsiveMenu">
-      <div class="topnav responsive">
-        <a href="/">
-          <img src="/logo_white.svg" style="max-width: 40px" />
-        </a>
-        <div class="toggle-icon responsive" @click="toggleMenu()">
-          <font-awesome-icon icon="times" />
-        </div>
-      </div>
-      <div class="menu responsive">
-        <a class="item text-md responsive " href="#news">News</a>
-        <a class="item text-md responsive" href="#contact">Contact</a>
-        <a class="item text-md responsive" href="#about">About</a>
-      </div>
-    </template>
-    <div v-else class="topnav">
-      <a href="/">
+    <template>
+      <div class="topnav">
         <img
           src="/logo_black.svg"
           :style="isMobile ? 'max-width: 40px' : 'max-width: 75px'"
         />
-      </a>
-      <mq-layout mq="tablet+">
-        <div class="menu">
-          <a class="item text-md" href="#news">News</a>
-          <a class="item text-md" href="#contact">Contact</a>
-          <a class="item text-md" href="#about">About</a>
-        </div>
-      </mq-layout>
-      <mq-layout mq="mobile">
-        <div class="toggle-icon" @click="toggleMenu()">
-          <font-awesome-icon icon="bars" />
-        </div>
-      </mq-layout>
-    </div>
+        <mq-layout mq="mobile">
+          <font-awesome-icon
+            v-if="showResponsiveMenu"
+            icon="times"
+            @click="toggleMenu()"
+          />
+          <font-awesome-icon v-else icon="bars" @click="toggleMenu()" />
+        </mq-layout>
+        <mq-layout mq="tablet+">
+          <a class="item text-sm" href="#news">News</a>
+          <a class="item text-sm" href="#contact">Contact</a>
+          <a class="item text-sm" href="#about">About</a>
+        </mq-layout>
+      </div>
+      <div v-if="showResponsiveMenu" class="responsive-menu">
+        <a class="responsive-item text-md" href="#news">News</a>
+        <a class="responsive-item text-md" href="#contact">Contact</a>
+        <a class="responsive-item text-md" href="#about">About</a>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -48,6 +39,13 @@ export default {
       return this.$mq === 'mobile'
     }
   },
+  watch: {
+    isMobile: function(currentIsMobile) {
+      if (!currentIsMobile) {
+        this.showResponsiveMenu = false
+      }
+    }
+  },
   methods: {
     toggleMenu() {
       this.showResponsiveMenu = !this.showResponsiveMenu
@@ -58,56 +56,46 @@ export default {
 
 <style scoped lang="scss">
 .navbar-container {
+  position: fixed;
+  top: 0;
+  width: calc(100vw - 50px);
+  max-width: 1200px;
+  padding-top: 10px;
+  background-color: white;
+  opacity: 1;
+  z-index: 999;
   display: flex;
   flex-direction: column;
   &.responsive {
-    background-color: black;
-    position: absolute;
-    top: 0;
-    left: 0;
     height: 100vh;
-    width: 100vw;
-    padding: 15px;
-    opacity: 1;
     visibility: visible;
-    z-index: 999;
     transition: opacity 0.15s linear, visibility 0s linear;
   }
-}
 
-.topnav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 15px 0;
-  &.responsive {
-    color: white;
+  .topnav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 5px;
+    border-bottom: 1px solid black;
+
+    .item {
+      text-decoration: none;
+      margin: 0 15px;
+    }
   }
-}
 
-.menu {
-  display: flex;
-  &.responsive {
+  .responsive-menu {
+    display: flex;
     flex-direction: column;
     align-items: center;
     flex-grow: 1;
     padding: 50px;
-    color: white;
-  }
-}
 
-.item {
-  text-decoration: none;
-  margin: 0 15px;
-  &.responsive {
-    color: white;
-    margin: 25px;
-  }
-}
-
-.toggle-icon {
-  &.responsive {
-    color: white;
+    .responsive-item {
+      text-decoration: none;
+      margin: 25px;
+    }
   }
 }
 </style>
