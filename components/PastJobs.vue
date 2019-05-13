10375db36a8215e1e3a0a2b1ee98dbfe0395f5b3
BenStirrup <benjamin.stirrup@gmail.com>
2019-05-13 20:18:29 +0200

    Add candriam logo to past work section

--- components/PastJobs.vue
+++ components/PastJobs.vue
@@ -5,30 +5,20 @@
       <div class="euler-hermes">
         <img
           src="/logos/euler_hermes_logo_black.svg"
-          style="max-width: 150px; max-height: 40px"
           alt="Logo of Euler Hermes"
         />
       </div>
+      <div class="candriam">
+        <img src="/logos/candriam_logo_black.svg" alt="Logo of Candriam" />
+      </div>
       <div class="sensome">
-        <img
-          src="/logos/sensome_logo_black.svg"
-          style="max-width: 150px; max-height: 40px"
-          alt="Logo of Sensome"
-        />
+        <img src="/logos/sensome_logo_black.svg" alt="Logo of Sensome" />
       </div>
       <div class="sipios">
-        <img
-          src="/logos/sipios_logo_black.svg"
-          style="max-width: 150px; max-height: 40px"
-          alt="Logo of Sipios"
-        />
+        <img src="/logos/sipios_logo_black.svg" alt="Logo of Sipios" />
       </div>
       <div class="theodo">
-        <img
-          src="/logos/theodo_logo_black.svg"
-          style="max-width: 150px; max-height: 25px"
-          alt="Logo of Theodo"
-        />
+        <img src="/logos/theodo_logo_black.svg" alt="Logo of Theodo" />
       </div>
     </div>
   </div>
@@ -49,9 +39,10 @@
   .logos {
     display: flex;
     flex-wrap: wrap;
-    width: 60%;
+    width: 70%;
 
     .euler-hermes,
+    .candriam,
     .sensome,
     .sipios,
     .theodo {
@@ -61,12 +52,34 @@
       align-items: center;
       width: 100%;
 
-      @include tablet-only {
-        width: 50%;
+      @include desktop-and-up {
+        width: 20%;
       }
 
-      @include desktop-and-up {
-        width: 25%;
+      img {
+        max-width: 150px;
+        max-height: 40px;
+      }
+    }
+
+    .theodo {
+      img {
+        max-height: 25px;
+      }
+    }
+
+    .euler-hermes,
+    .candriam,
+    .sensome {
+      @include tablet-only {
+        width: calc(100% / 3);
+      }
+    }
+
+    .sipios,
+    .theodo {
+      @include tablet-only {
+        width: 50%;
       }
     }
   }
