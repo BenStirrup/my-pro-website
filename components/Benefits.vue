762c752aa4b4262a254df5fd29b43ac008b63467
BenStirrup <benjamin.stirrup@gmail.com>
2019-05-16 00:08:59 +0200

    Improve responsive of Benefits section

--- components/Benefits.vue
+++ components/Benefits.vue
@@ -1,33 +1,34 @@
 <template>
   <div class="benefits-container">
     <div class="title text-lg semi-bold">What I offer you</div>
-    <div class="benefit">
-      <img
-        class="benefit-illustration"
-        src="/benefits/undraw_business_deal.svg"
-        alt="Drawing of two people shaking their hands in agreement"
-      />
-      <div class="benefit-title text-sm semi-bold">
-        A team member who challenges your business
+    <div class="benefits">
+      <div class="benefit">
+        <img
+          src="/benefits/undraw_business_deal.svg"
+          alt="Drawing of two people shaking their hands in agreement"
+        />
+        <div class="benefit-title text-sm semi-bold">
+          A team member who challenges your business
+        </div>
+        <div class="benefit-description text-xs">
+          I will challenge with you the definition and the prioritization of
+          high valued-added features for your business and which your users
+          need.
+        </div>
       </div>
-      <div class="benefit-description text-xs">
-        I will challenge with you the definition and the prioritization of high
-        valued-added features for your business and which your users need.
-      </div>
-    </div>
-    <div class="benefit">
-      <img
-        class="benefit-illustration"
-        src="/benefits/undraw_finish_line.svg"
-        alt="Drawing of a woman who just finished a running race"
-      />
-      <div class="benefit-title text-sm semi-bold">
-        Build at lightspeed with Agile and DevOps mindsets
-      </div>
-      <div class="benefit-description text-xs">
-        To build a product in weeks, not months, I ship code as early and as
-        regularly as possible, getting user feedback early on and constantly
-        measuring the impact of what I am building.
+      <div class="benefit">
+        <img
+          src="/benefits/undraw_finish_line.svg"
+          alt="Drawing of a woman who just finished a running race"
+        />
+        <div class="benefit-title text-sm semi-bold">
+          Build at lightspeed with Agile and DevOps mindsets
+        </div>
+        <div class="benefit-description text-xs">
+          To build a product in weeks, not months, I ship code as early and as
+          regularly as possible, getting user feedback early on and constantly
+          measuring the impact of what I am building.
+        </div>
       </div>
     </div>
   </div>
@@ -38,57 +39,54 @@
 
 .benefits-container {
   display: flex;
-  flex-wrap: wrap;
-  margin: auto;
+  flex-direction: column;
+  align-items: center;
 
   .title {
-    width: 100%;
-    text-align: center;
     padding-bottom: 40px;
   }
 
-  .benefit {
+  .benefits {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: auto;
-    padding: 20px;
-    width: 90%;
+    flex-wrap: wrap;
 
-    @include desktop-and-up {
-      width: 50%;
-      height: 520px; // Absolute value so that benefit titles are aligned in the flex line
-    }
+    .benefit {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
 
-    .benefit-illustration {
-      padding-bottom: 20px;
-      max-width: 300px;
-      max-height: 300px;
-
-      @include mobile-only {
-        max-width: 225px;
-        max-height: 225px;
-      }
-
-      @include tablet-only {
-        max-width: 250px;
-        max-height: 250px;
+      > * {
+        padding: 15px 0;
       }
 
       @include desktop-and-up {
-        width: 300px; // Absolute value so that benefit titles are aligned in the flex line
-        height: 300px;
+        width: 50%;
+        height: 550px; // Absolute value so that benefit titles are aligned in the flex line
       }
-    }
 
-    .benefit-title {
-      text-align: center;
-      padding-bottom: 20px;
-    }
+      img {
+        height: 200px;
 
-    .benefit-description {
-      text-align: justify;
-      width: 90%;
+        @include tablet-and-up {
+          height: 275px;
+        }
+      }
+
+      .benefit-title {
+        text-align: center;
+        width: 80%;
+        @include tablet-only {
+          width: 60%;
+        }
+      }
+
+      .benefit-description {
+        text-align: justify;
+        width: 80%;
+        @include tablet-only {
+          width: 60%;
+        }
+      }
     }
   }
 }
